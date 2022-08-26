42e2d2ce3811a537e05c7421286ff3912a0635b7
Mamedov_off <mamedov_dev@mail.ru>
2022-08-26 18:14:39 +0300

    ApexChart

--- src/page/Convert.tsx
+++ src/page/Convert.tsx
@@ -1,11 +1,40 @@
+import Chart from 'react-apexcharts'
+import { useEffect, useMemo } from 'react'
+import { ApexOptions } from 'apexcharts'
+
 const Convert = () => {
+  useEffect(() => {
+    console.log('render')
+  })
+  const options = useMemo(
+    () => ({
+      options: {
+        chart: {
+          animations: {
+            enabled: false,
+          },
+          id: 'basic-bar',
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      series: [
+        {
+          name: 'series-1',
+          data: [30, 40, 45, 50, 49, 60, 70, 91],
+        },
+      ],
+    }),
+    [],
+  )
   return (
-    <div>
-      <h1>200</h1>
-      <div className="d-flex">
-        <button className="btn btn-primary">Add cash</button>
-        <button className="btn btn-primary">Get Cash</button>
-      </div>
+    <div className="d-flex justify-content-center mt-5 flex-wrap">
+      <Chart options={options.options} series={options.series} type="bar" width="500" />
+      <Chart options={{}} series={[44, 55, 41, 17, 15]} type="donut" width="380" />
     </div>
   )
 }
