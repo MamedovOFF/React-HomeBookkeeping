c737a6109d60f58751ce247dc28d0d17b1fe5433
rmamedov <mamedov_dev@mail.ru>
2022-09-06 10:31:49 +0300

    Fix Firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,7 +8,6 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 }
 
 initializeApp(firebaseConfig)
